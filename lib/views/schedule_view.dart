9fb1beaafcbcfa732b0d5125140c3b6e6b1cb0eb
yoonus47 <yoonuskottai@gmail.com>
2022-07-26 19:21:23 +0530

    added sfcalendar

--- lib/views/schedule_view.dart
+++ lib/views/schedule_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 import 'package:travelaza/models/Trip.dart';
 
@@ -14,86 +15,101 @@ class ScheduleView extends StatefulWidget {
 }
 
 class _ScheduleViewState extends State<ScheduleView> {
-  DatePickerController _controller = DatePickerController();
-
-  DateTime _selectedDay = DateTime.now();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromARGB(255, 245, 245, 247),
         appBar: AppBar(
+          title: Text('Manage Schedule'),
           automaticallyImplyLeading: false,
           leading: IconButton(
             onPressed: () => Navigator.of(context).pop(),
             icon: new Icon(Icons.arrow_back),
           ),
         ),
-        body: Column(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(top: 20, bottom: 20),
-                  child: DatePicker(
-                    widget.trip.startDate,
-                    width: 60,
-                    height: 80,
-                    controller: _controller,
-                    initialSelectedDate: widget.trip.startDate,
-                    daysCount: widget.trip.endDate
-                        .difference(widget.trip.startDate)
-                        .inDays,
-                    selectionColor: Color.fromARGB(255, 2, 59, 89),
-                    selectedTextColor: Colors.white,
-                    onDateChange: (date) {
-                      // New date selected
-                      setState(() {
-                        _selectedDay = date;
-                      });
-                    },
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
-                  color: Color.fromARGB(255, 234, 232, 236),
-                  child: Column(
-                    children: [
-                      Text(
-                        widget.trip.title,
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 2, 59, 89),
-                          fontSize: 30,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                      ),
-                      Text(
-                        "You have selected",
-                        style: TextStyle(
-                            fontSize: 25.0,
-                            color: Color.fromARGB(255, 2, 59, 89)),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                      ),
-                      Text(DateFormat('MMMMd').format(_selectedDay).toString(),
-                          style: TextStyle(
-                              fontSize: 25.0,
-                              color: Color.fromARGB(255, 2, 59, 89))),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
+        body: SfCalendar(
+          // allowedViews: [CalendarView.day,CalendarView.week,CalendarView.workWeek],
+          // showNavigationArrow: true,
+          // showDatePickerButton: true,
+          backgroundColor: Color.fromARGB(255, 246, 235, 244),
+          minDate: widget.trip.startDate,
+          maxDate: widget.trip.endDate,
+          cellBorderColor: Colors.transparent,
+
+          // allowViewNavigation: true,
         ));
   }
 }
+
+
+
+  // DatePickerController _controller = DatePickerController();
+
+  // DateTime _selectedDay = DateTime.now();
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  // }
+
+      // Column(
+      //   children: [
+      //     Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: <Widget>[
+      //         Container(
+      //           padding: EdgeInsets.only(top: 20, bottom: 20),
+      //           child: DatePicker(
+      //             widget.trip.startDate,
+      //             width: 60,
+      //             height: 80,
+      //             controller: _controller,
+      //             initialSelectedDate: widget.trip.startDate,
+      //             daysCount: widget.trip.endDate
+      //                 .difference(widget.trip.startDate)
+      //                 .inDays,
+      //             selectionColor: Color.fromARGB(255, 2, 59, 89),
+      //             selectedTextColor: Colors.white,
+      //             onDateChange: (date) {
+      //               // New date selected
+      //               setState(() {
+      //                 _selectedDay = date;
+      //               });
+      //             },
+      //           ),
+      //         ),
+      //         Container(
+      //           padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
+      //           color: Color.fromARGB(255, 234, 232, 236),
+      //           child: Column(
+      //             children: [
+      //               Text(
+      //                 widget.trip.title,
+      //                 style: TextStyle(
+      //                   color: Color.fromARGB(255, 2, 59, 89),
+      //                   fontSize: 30,
+      //                 ),
+      //               ),
+      //               Padding(
+      //                 padding: EdgeInsets.all(10),
+      //               ),
+      //               Text(
+      //                 "You have selected",
+      //                 style: TextStyle(
+      //                     fontSize: 25.0,
+      //                     color: Color.fromARGB(255, 2, 59, 89)),
+      //               ),
+      //               Padding(
+      //                 padding: EdgeInsets.all(10),
+      //               ),
+      //               Text(DateFormat('MMMMd').format(_selectedDay).toString(),
+      //                   style: TextStyle(
+      //                       fontSize: 25.0,
+      //                       color: Color.fromARGB(255, 2, 59, 89))),
+      //             ],
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ],
+      // )
