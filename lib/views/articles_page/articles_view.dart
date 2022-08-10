f682ee9c474246a89ebbd268e7421e63576cabc0
yoonus47 <yoonuskottai@gmail.com>
2022-08-10 20:37:03 +0530

    Added article box

--- lib/views/articles_page/articles_view.dart
+++ lib/views/articles_page/articles_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class ArticlesView extends StatelessWidget {
   @override
@@ -24,6 +25,64 @@ class ArticlesView extends StatelessWidget {
           child: Icon(Icons.add),
           backgroundColor: Color(0xFF1A395A),
           onPressed: () {}),
+      body: Container(
+        // trip.getLocationImage()
+        height: 300,
+        padding: EdgeInsets.fromLTRB(6, 8, 6, 8),
+        child: Card(
+          color: Color.fromARGB(255, 246, 235, 244),
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: InkWell(
+            child: Container(
+              color: Color.fromARGB(255, 76, 191, 166),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 16.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 40.0, 0, 35.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: AutoSizeText(
+                              'My Dubai Experience',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25.0,
+                                color: Color.fromARGB(255, 2, 59, 89),
+                              ),
+                              maxLines: 3,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 30.0, 0, 8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "John Lewis",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22.0,
+                              color: Color.fromARGB(255, 2, 59, 89),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            onTap: () {},
+          ),
+        ),
+      ),
     );
   }
 }
