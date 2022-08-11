2b584e8beb7e95e4dea4db0c1cb7917cd4f6e6df
yoonus47 <yoonuskottai@gmail.com>
2022-08-11 22:28:53 +0530

    more minor modifs to article page

--- lib/views/articles_page/articles_view.dart
+++ lib/views/articles_page/articles_view.dart
@@ -201,6 +201,89 @@ class ArticlesView extends StatelessWidget {
                 ),
               ),
             ),
+            Container(
+              // trip.getLocationImage()
+              height: 350,
+              padding: EdgeInsets.fromLTRB(6, 8, 6, 8),
+              child: Card(
+                color: Color.fromARGB(255, 246, 235, 244),
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: InkWell(
+                  child: Container(
+                    color: Color.fromARGB(255, 234, 232, 236),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 16.0),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(0, 40.0, 0, 10.0),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: AutoSizeText(
+                                    'Why Its the Perfect Time to visit Ladakh',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 25.0,
+                                      color: Color.fromARGB(255, 2, 59, 89),
+                                    ),
+                                    maxLines: 3,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 5.0, 0, 15.0),
+                            child: Row(
+                              children: [
+                                Text(
+                                  "Unni Kuttan",
+                                  textAlign: TextAlign.right,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 22.0,
+                                    color: Color.fromARGB(255, 2, 59, 89),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 5.0),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: AutoSizeText(
+                                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been since the 1500s ...',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w200,
+                                      fontSize: 20.0,
+                                      color: Color.fromARGB(255, 2, 59, 89),
+                                    ),
+                                    maxLines: 4,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => InDev()),
+                    );
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),
