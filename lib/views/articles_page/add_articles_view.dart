ab44b922d3091d753db14b7631a881d6b3e9e302
yoonus47 <yoonuskottai@gmail.com>
2022-08-13 22:01:46 +0530

    able to select gallery images to add article page

--- lib/views/articles_page/add_articles_view.dart
+++ lib/views/articles_page/add_articles_view.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddArticlesView extends StatefulWidget {
   const AddArticlesView({Key? key}) : super(key: key);
@@ -9,6 +11,23 @@ class AddArticlesView extends StatefulWidget {
 }
 
 class _AddArticlesViewState extends State<AddArticlesView> {
+  final TextEditingController _descriptionEditingControlller =
+      TextEditingController();
+
+  File? _image;
+
+  final _picker = ImagePicker();
+  // Implementing the image picker
+  Future<void> _openImagePicker() async {
+    final XFile? pickedImage =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,19 +38,54 @@ class _AddArticlesViewState extends State<AddArticlesView> {
         padding: const EdgeInsets.all(14.0),
         child: Column(
           children: [
-            Container(
+            _image == null
+                ? Container(
+                    width: double.infinity,
+                    height: 250,
+                    color: Colors.grey,
+                    child: Center(
+                      child: RaisedButton(
+                          onPressed: () {
+                            _openImagePicker();
+                          },
+                          color: Colors.pink,
+                          child: Text(
+                            'Choose Image',
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          )),
+                    ))
+                : GestureDetector(
+                    onTap: () {
+                      _openImagePicker();
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: 250,
+                      child: _image != null
+                          ? Image.file(_image!, fit: BoxFit.cover)
+                          : const Text('Please select an image'),
+                    ),
+                  ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _descriptionEditingControlller,
+              keyboardType: TextInputType.text,
+              decoration: (InputDecoration(labelText: 'Description')),
+            ),
+            SizedBox(height: 40),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                color: Colors.pink,
                 width: double.infinity,
-                height: 25,
-                color: Colors.grey,
+                height: 50,
                 child: Center(
-                  child: RaisedButton(
-                      onPressed: () {},
-                      color: Colors.pink,
-                      child: Text(
-                        'Upload Image',
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      )),
-                ))
+                    child: Text(
+                  'Add New Post',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                )),
+              ),
+            )
           ],
         ),
       ),
