4e780a56043bae020f9649b54a67a0aaca38ca72
yoonus47 <yoonuskottai@gmail.com>
2022-08-27 22:58:54 +0530

    still working on discover page

--- lib/views/discover_page/discover_view.dart
+++ lib/views/discover_page/discover_view.dart
@@ -5,28 +5,34 @@ class DiscoverView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 246, 235, 244),
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Color.fromARGB(255, 246, 235, 244),
-        elevation: 0.0,
-        centerTitle: false,
-        title: Text(
-          'Discover',
-          style: TextStyle(
-              color: Color.fromARGB(255, 2, 59, 89),
-              fontSize: 28,
-              fontWeight: FontWeight.w600),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(17, 12, 6, 12),
-        child: Text(
-          'This page is under developement',
-          style: TextStyle(
-            color: Color.fromARGB(255, 2, 59, 89),
-            fontSize: 30,
-            fontWeight: FontWeight.w400,
-          ),
+      // appBar: AppBar(
+      //   automaticallyImplyLeading: false,
+      //   backgroundColor: Color.fromARGB(255, 246, 235, 244),
+      //   elevation: 0.0,
+      //   centerTitle: false,
+      //   title: Text(
+      //     'Discover',
+      //     style: TextStyle(
+      //         color: Color.fromARGB(255, 2, 59, 89),
+      //         fontSize: 28,
+      //         fontWeight: FontWeight.w600),
+      //   ),
+      // ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 18, 0, 0),
+              child: Text(
+                'Welcome Yoonus!',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 2, 59, 89),
+                  fontSize: 35,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
