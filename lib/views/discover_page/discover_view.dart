faf20eb187886cfe507ff3d34e169276de5eca24
yoonus47 <yoonuskottai@gmail.com>
2022-08-31 20:36:25 +0530

    Added convert currency widget in discover page

--- lib/views/discover_page/discover_view.dart
+++ lib/views/discover_page/discover_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travelaza/views/indev.dart';
 
 class DiscoverView extends StatelessWidget {
   @override
@@ -42,12 +43,47 @@ class DiscoverView extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset(
-                  'images/weather-c.gif',
-                  width: double.infinity,
+              child: InkWell(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'images/weather-c.gif',
+                    width: double.infinity,
+                  ),
                 ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => InDev()));
+                },
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              color: Color.fromARGB(255, 2, 59, 89),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: InkWell(
+                child: Container(
+                  height: 85,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(17, 19, 17, 15),
+                    child: Text(
+                      'Convert Currencies',
+                      style: TextStyle(
+                          fontSize: 33,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFFF9DF90)),
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => InDev()));
+                },
               ),
             )
           ],
